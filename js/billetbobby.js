6d8562ca9b501fddba4568b068fc195aa85b3a8b
20100lefebvre <vinct_930@hotmail.com>
2021-08-13 11:14:45 +0200

    13 aout 2021 envoi site de base bd diaporama

---
+++ js/billetbobby.js
@@ -0,0 +1,17 @@
+code = prompt("Entrez le mot de passe: 'Bobby',s'il vous plaît.(Ce site est en période de tests)");
+if (code !== "Bobby")
+{
+    location.href="erreur.html";//ici c'est la page où est redirigé le visiteur qui entre le mauvais mot de passe
+}
+
+function imgSlider(anything){
+    document.getElementById("slider").src = anything;
+}
+
+//========================//
+//© 2021 Vincent Lefebvre //
+//========================//
+
+let currentYear = new Date().getFullYear();
+let c = document.querySelector(".copyright");
+c.innerText = "© " + currentYear + " Vincent lefebvre";
