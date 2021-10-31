9da44e9d2d19ab10f2a22cb010a6ad171955f999
20100lefebvre <vinct_930@hotmail.com>
2021-10-31 20:04:44 +0100

    slider remis à jour

--- js/billetbobby.js
+++ js/billetbobby.js
@@ -1,8 +1,10 @@
+/*
 code = prompt("Entrez le mot de passe: 'Bobby',s'il vous plaît.(Ce site est en période de tests)");
 if (code !== "Bobby")
 {
     location.href="erreur.html";//ici c'est la page où est redirigé le visiteur qui entre le mauvais mot de passe
 }
+*/
 
 function imgSlider(anything){
     document.getElementById("slider").src = anything;
